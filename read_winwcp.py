d7e80dbc7ed10046ec3839a20f7e782df01e59a6
Marcel Goldschen-Ohm <goldschen-ohm@utexas.edu>
2025-03-21 00:13:46 -0500

    read into xarray.Dataset

--- read_winwcp.py
+++ read_winwcp.py
@@ -1,10 +1,16 @@
 
 import struct
 import numpy as np
-from datetime import datetime, date, time
+import xarray as xr
+# from datetime import datetime, date, time
 
-def read_winwcp(filepath: str):
-    """Read data from a WinWCP file."""
+def read_winwcp(filepath: str) -> xr.Dataset:
+    """Read data from a WinWCP file into an xarray.Dataset.
+    
+    Assumes all sweeps have the same time coordinates.
+    If not, it is up to you to use the sweep_sample_interval coordinate
+    to separate sweeps into individual xarray.DataArrays with different time coordinates.
+    """
 
     # read file contents as binary byte array
     with open(filepath, mode='rb') as file:
@@ -26,6 +32,19 @@ def read_winwcp(filepath: str):
             header[k] = v
         except:
             pass
+    
+    # datetime
+    date = header['CTIME'].split(' ')[0].strip()
+    month, day, year = date.split('-')
+    if len(year) == 2:
+        year = f'20{year}'
+    if len(month) == 1:
+        month = f'0{month}'
+    if len(day) == 1:
+        day = f'0{day}'
+    date = f'{year}-{month}-{day}'
+    timestamp = header['RTIME'].split(' ')[-1].strip()
+    datetime = f'{date} {timestamp}'
 
     # read records (i.e., sweeps)
     # Each record consists of an analysis block followed by a data block.
@@ -39,38 +58,55 @@ def read_winwcp(filepath: str):
         n_samples = int(header['NP'])
     except:
         n_samples = int(n_data_bytes / 2 / n_channels)
-
-    # store everything in a dict
-    data = {}
-    data['winwcp_header'] = header
-    data['sample_interval_sec'] = float(header['DT'])
-    data['channel_names'] = np.array([header[f'YN{i}'] for i in range(n_channels)], dtype=object)
-    data['channel_units'] = np.array([header[f'YU{i}'] for i in range(n_channels)], dtype=object)
-    data['digitized_signal'] = np.zeros((n_sweeps, n_channels, n_samples), dtype=np.int16)
-    data['channel_physical_signal_conversion_factors'] = np.ones(n_channels)
-    data['sweep_status'] = np.array([''] * n_sweeps, dtype=object)
-    data['sweep_type'] = np.array([''] * n_sweeps, dtype=object)
-    data['sweep_group'] = np.zeros(n_sweeps)
-    data['sweep_start_time_sec'] = np.zeros(n_sweeps)
-    data['sweep_sample_interval_sec'] = np.zeros(n_sweeps)
     
-    # datetime
-    date = header['CTIME'].split(' ')[0].strip()
-    month, day, year = date.split('-')
-    if len(year) == 2:
-        year = f'20{year}'
-    if len(month) == 1:
-        month = f'0{month}'
-    if len(day) == 1:
-        day = f'0{day}'
-    date = f'{year}-{month}-{day}'
-    timestamp = header['RTIME'].split(' ')[-1].strip()
-    data['date'] = date
-    data['datetime'] = f'{date} {timestamp}'
+    # store everything in a xarray.Dataset
+    channel_names = [header[f'YN{i}'] for i in range(n_channels)]
+    channel_units = [header[f'YU{i}'] for i in range(n_channels)]
+    data = xr.Dataset(
+        data_vars={
+            channel_names[i]: xr.DataArray(
+                data=np.zeros((n_sweeps, n_samples)),
+                dims=['sweep', 'time'], 
+                attrs={'units': channel_units[i]})
+            for i in range(n_channels)
+        },
+        coords={
+            'sweep': np.arange(1, n_sweeps + 1), # 1-based sweep index
+            'time': xr.DataArray(
+                data=np.arange(n_samples) * float(header['DT']), 
+                dims=['time'], 
+                attrs={'units': 's'}),
+            'sweep_status': xr.DataArray(
+                data=np.array(['ACCEPTED'] * n_sweeps, dtype=object), 
+                dims=['sweep']),
+            'sweep_type': xr.DataArray(
+                data=np.array(['TEST'] * n_sweeps, dtype=object), 
+                dims=['sweep']),
+            'sweep_group': xr.DataArray(
+                data=np.zeros(n_sweeps), 
+                dims=['sweep']),
+            'sweep_start_time': xr.DataArray(
+                data=np.zeros(n_sweeps), 
+                dims=['sweep'], 
+                attrs={'units': 's'}),
+            # Just in case the sample interval is different for each sweep.
+            # If so, it is up to you to split this Dataset into individual DataArrays
+            # with appropriate time coordinates.
+            'sweep_sample_interval': xr.DataArray(
+                data=np.zeros(n_sweeps), 
+                dims=['sweep'], 
+                attrs={'units': 's'}),
+        },
+        attrs={
+            'date': date,
+            'datetime': datetime,
+            'winwcp_header': header,
+        }
+    )
 
-    # needed for converting digitized signal to signal in physical units
+    # for converting digitized signal to signal in physical units
     ADCmax = int(header['ADCMAX'])
-    gain_per_channel = np.array([float(header[f'YG{i}']) for i in range(n_channels)])#.reshape(n_channels, 1)
+    gain_per_channel = np.array([float(header[f'YG{i}']) for i in range(n_channels)]).reshape(n_channels, 1)
 
     for i in range(n_sweeps):
         n_offset_bytes = n_header_bytes + i * (n_analysis_bytes + n_data_bytes)
@@ -80,18 +116,19 @@ def read_winwcp(filepath: str):
         data['sweep_status'][i] = analysis_bytes[:8].decode('utf-8')
         data['sweep_type'][i] = analysis_bytes[8:12].decode('utf-8')
         data['sweep_group'][i] = struct.unpack('f', analysis_bytes[12:16])[0]
-        data['sweep_start_time_sec'][i] = struct.unpack('f', analysis_bytes[16:20])[0]
-        data['sweep_sample_interval_sec'][i] = struct.unpack('f', analysis_bytes[20:24])[0]
-        Vmax_per_channel = np.array(struct.unpack('f'*n_channels, analysis_bytes[24:24+4*n_channels]))#.reshape(n_channels, 1)
-        data['channel_physical_signal_conversion_factors'][i] = Vmax_per_channel
+        data['sweep_start_time'][i] = struct.unpack('f', analysis_bytes[16:20])[0]
+        data['sweep_sample_interval'][i] = struct.unpack('f', analysis_bytes[20:24])[0]
+        Vmax_per_channel = np.array(struct.unpack('f'*n_channels, analysis_bytes[24:24+4*n_channels])).reshape(n_channels, 1)
 
-        # digitized data as 16-bit signed integers -> (channel, sample)
+        # digitized sweep as 16-bit signed integers -> (channel, sample)
         n_pts = int(n_samples * n_channels)
         digitized_sweep = np.array(struct.unpack('h'*n_pts, data_bytes[:2*n_pts])).reshape(n_samples, n_channels).T
-        data['digitized_signal'][i] = digitized_sweep
         
-        # calibrated signal in physical units
-        data['channel_data'][i] = (Vmax_per_channel / (ADCmax * gain_per_channel)) * digitized_signal
+        # calibrated signal in physical units (channel, sample)
+        calibrated_sweep = (Vmax_per_channel / (ADCmax * gain_per_channel)) * digitized_sweep
+
+        for j in range(n_channels):
+            data[channel_names[j]][i] = calibrated_sweep[j]
     
     return data
 
@@ -100,16 +137,12 @@ if __name__ == '__main__':
     import matplotlib.pyplot as plt
 
     filepath = 'your/path/to/file.wcp'  # change this
+    filepath = '250313_001.Pstep500ms.12.wcp'
     data = read_winwcp(filepath)
-    n_sweeps, n_channels, n_samples = data['channel_data'].shape
-    time_sec = np.arange(n_samples) * data['sample_interval_sec']
+    print(data)
 
-    # plot the sweep average
-    fig, ax = plt.subplots(nrows=n_channels, ncols=1)
-    for i in range(n_channels):
-        ax[i].plot(time_sec, data['channel_data'][:,i].mean(axis=0), lw=1)
-        ax[i].set_ylabel(f'{data["channel_names"][i]} ({data["channel_units"][i]})')
-    ax[-1].set_xlabel('Time (s)')
-    ax[0].set_title(data["datetime"])
+    for i, name in enumerate(data.data_vars):
+        plt.subplot(len(data.data_vars), 1, i + 1)
+        data[name].mean(dim='sweep').plot()
     plt.tight_layout()
     plt.show()
